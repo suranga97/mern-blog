0e7949a3f1e35930d1e2dff3dd72919175fedea9
suranga97 <suranga@thesanmark.com>
2024-04-05 12:32:47 +0530

    #9 connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Mongo is connected');
+  }).catch(err =>{
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(8000, () => { 
-
-    console.log('Server is running on port 8000!!');
+app.listen(8000, () => {
+  console.log('Server is running on port 8000!!');
 });
